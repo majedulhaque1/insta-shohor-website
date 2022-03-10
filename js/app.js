1f3156717f63c347c85fb2fa4c61aca24698d33c
majedulhaque1 <majedulhaquerakib601@gmail.com>
2022-03-11 00:11:08 +0600

    user name and comment bug fixed

--- js/app.js
+++ js/app.js
@@ -26,7 +26,6 @@ const reportPost = (id) => {
 };
 
 const displayContent = (text) => {
-  console.log(text);
     return text.length < 30 ? text : text.slice(0, 30) + "<span class='fw-bold'>... read more</span>";
 };
 
@@ -51,6 +50,7 @@ const switchTab = (id) => {
 };
 
 const createPost = (post) => {
+  console.log(post.comments);
     const image = post.image;
     const div = document.createElement( "article" );
     div.classList.add( "post" );
@@ -120,9 +120,9 @@ const createPost = (post) => {
                   <div class="post__description">
                     <small>
                       <a class="post__name--underline" href="#">
-                          ${post.comments?.user}
+                          ${post.comments[0]?.user}
                       </a>
-                      ${post.comments?.text}
+                      ${post.comments[0]?.text}
                     </small>
                   </div>
                   <span class="post__date-time">30 minutes ago</span>
